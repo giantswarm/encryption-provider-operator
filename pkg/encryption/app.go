7d22bd76ee2650dae66c7b89aaf3654f44e39ff2
calvix <vaclav@giantswarm.io>
2022-01-21 14:15:31 +0100

    fmt

--- pkg/encryption/app.go
+++ pkg/encryption/app.go
@@ -3,12 +3,12 @@ package encryption
 import (
 	"context"
 	"fmt"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
 
 	chartv1 "github.com/giantswarm/apiextensions-application/api/v1alpha1"
 	"github.com/giantswarm/k8smetadata/pkg/annotation"
 	"github.com/giantswarm/k8smetadata/pkg/label"
 	v1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/giantswarm/encryption-provider-operator/pkg/project"
